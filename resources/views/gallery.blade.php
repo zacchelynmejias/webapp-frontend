a6cff7c008eea50ffad19d977eb67e7ee36580b4
zacchelynmejias <zacchelynmejias@gmail.com>
2022-11-02 02:05:49 +0800

    Added Gallery Page and Setup Blade  Directives

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+            <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+
+            <h1 class="text-white font-weight-bold">Gallery</h1>
+            </div>
+        </header>
+
+@endsection
