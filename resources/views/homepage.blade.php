05cdc151b36204014d4299bfbef3329774d03546
zacchelynmejias <zacchelynmejias@gmail.com>
2022-12-22 15:01:02 +0800

    changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,7 @@
         
         <!-- Masthead-->
         <header class="masthead">
+            <div class="header-image"></div>
             <div class="container px-4 px-lg-5 h-100">
                 <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-8 align-self-end">
@@ -14,11 +15,12 @@
                     </div>
                     <div class="col-lg-8 align-self-baseline">
                         <p class="text-white-75 mb-5"> Be Ready For New & Better You!</p>
-
                     </div>
                 </div>
             </div>
         </header>
+
+            
         
         <!-- Announcement-->
         <section class="page-section bg-primary" id="announcement">
@@ -26,8 +28,7 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 text-center">
                         <h2 class="text-white mt-0">We've got what you need!</h2>
-                        <hr class="divider divider-light" />
-                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                        <hr class="divider divider-light" />                 
                          
             <body> 
             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
