287c14fa6bfe7ac97e21d112fb34569eb9e03bd4
zacchelynmejias <zacchelynmejias@gmail.com>
2022-12-22 15:49:23 +0800

    ...

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -29,40 +29,31 @@
                     <div class="col-lg-8 text-center">
                         <h2 class="text-white mt-0">We've got what you need!</h2>
                         <hr class="divider divider-light" />                 
-                         
-            <body> 
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-             <div class="carousel-indicators">
-                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-             </div>
-                 <div class="carousel-inner">
-                  <div class="carousel-item active">
-               <img src="..." class="d-block w-100" alt="...">
-            </div>
-                <div class="carousel-item">
-                 <img src="..." class="d-block w-100" alt="...">
-           </div>
-               <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-          </div>
-         </div>
-             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-           <span class="visually-hidden">Previous</span>
-         </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-           <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-             </button>
-
+                         <br>
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                           <div class="carousel-inner">
+                             <div class="carousel-item active">
+                          <img src="..." class="d-block w-100" alt="...">
+                         </div>
+                             <div class="carousel-item">
+                         <img src="..." class="d-block w-100" alt="...">
+                        </div>
+                             <div class="carousel-item">
+                            <img src="..." class="d-block w-100" alt="...">
+                     </div>
+                 </div>
+                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="visually-hidden">Previous</span>
+                    </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                     </button>
                     </div>
-                </div>
-            </div>
-            </div>
-</body>
-        </section>
+                   </section>
+
+
         <!-- About Us-->
         <section class="page-section" id="about us">
             <div class="container px-4 px-lg-5">
